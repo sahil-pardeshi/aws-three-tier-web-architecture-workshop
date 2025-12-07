f9a09ca853d3220353e0d41e09bcce8cd5fa30ce
sahil-pardeshi <sahilpardeshi205@gmail.com>
2025-12-07 14:51:22 +0530

    Update DbConfig.js
    this is cm1

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1',
+    DB_USER : 'admin',
+    DB_PWD : 'pass1234',
+    DB_DATABASE : 'webappdb'
 });
